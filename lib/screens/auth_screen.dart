adc1d0ef6a7c9f12ce7ba33926a77ba5458ff0bf
a7md0 <a7m3d699@gmail.com>
2021-03-05 23:49:46 +0300

    Remove unused import

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:chat_app/widgets/auth/auth_form.dart';
