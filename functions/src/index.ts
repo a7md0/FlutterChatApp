07b1b8818bb079809f71fa14628d22d26b65dd0e
a7md0 <a7m3d699@gmail.com>
2021-03-06 00:47:57 +0300

    Add functions trigger

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+
+exports.notificationFunction = functions.firestore
+    .document("chat/{message}")
+    .onCreate((snapshot, context) => {
+      const data = snapshot.data();
+
+      return admin.messaging().sendToTopic("chat", {
+        notification: {
+          title: data.username,
+          body: data.text,
+          clickAction: "FLUTTER_NOTIFICATION_CLICK",
+        },
+      });
+    });
