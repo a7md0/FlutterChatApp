f55d40ba7ffdb04600adfc6e2910854e9c0741ca
a7md0 <a7m3d699@gmail.com>
2021-03-05 16:48:41 +0300

    AuthForm validate and submit

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -11,6 +11,14 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _isLogin = true;
+
+  String _userEmail = "";
+  String _userName = "";
+  String _userPassword = "";
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -20,35 +28,66 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                     ),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+
+                      return null;
+                    },
+                    onSaved: (value) => _userEmail = value,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 character';
+                        }
+
+                        return null;
+                      },
+                      onSaved: (value) => _userName = value,
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('password'),
                     keyboardType: TextInputType.visiblePassword,
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long';
+                      }
+
+                      return null;
+                    },
+                    onSaved: (value) => _userPassword = value,
                   ),
                   SizedBox(height: 12),
                   RaisedButton(
-                    child: Text('Login'),
-                    onPressed: () {},
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
+                    onPressed: _trySubmit,
                   ),
                   TextButton(
-                    child: Text('Create new account'),
-                    onPressed: () {},
+                    child: Text(_isLogin ? 'Create new account' : 'I already have an account'),
+                    onPressed: () {
+                      setState(() => _isLogin = !_isLogin);
+                    },
                   ),
                 ],
               ),
@@ -57,5 +96,18 @@ class _AuthFormState extends State<AuthForm> {
         ),
       ),
     );
+  }
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+
+    FocusScope.of(context).unfocus(); // Close keyboard (remove focus from any input)
+
+    if (isValid) {
+      _formKey.currentState.save();
+
+      print(_userEmail);
+      print(_userName);
+    }
   }
 }
