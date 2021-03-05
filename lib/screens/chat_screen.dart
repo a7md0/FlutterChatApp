6eed71bb07d7a4875acf1e5976c213a8956e3738
a7md0 <a7m3d699@gmail.com>
2021-03-05 16:14:30 +0300

    Firestore query/add

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, idx) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This work $idx'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection(
+              '/chats/X9Ym9cELPzbUe0OAwVDX/messages',
+            )
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents as List<DocumentSnapshot>;
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, idx) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[idx]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection(
+            '/chats/X9Ym9cELPzbUe0OAwVDX/messages',
+          )
+              .add(
+            {
+              'text': 'New text',
+            },
+          );
+        },
       ),
     );
   }
