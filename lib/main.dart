8faae06cd80fe7989669deeb6f72c2e9aa4f585d
a7md0 <a7m3d699@gmail.com>
2021-03-05 17:22:32 +0300

    home page change based on auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:chat_app/screens/auth_screen.dart';
 
@@ -23,7 +25,16 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          }
+
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
