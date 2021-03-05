e7f3191fb809ba9334e97ce35aa845612cd0dcfc
a7md0 <a7m3d699@gmail.com>
2021-03-05 18:15:12 +0300

    Message bubble change dir based on sender

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  final Key key;
   final String message;
   final bool isMe;
 
   MessageBubble({
+    this.key,
     this.message,
     this.isMe,
   });
@@ -12,11 +14,17 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: Radius.circular(!isMe ? 0 : 12),
+              bottomRight: Radius.circular(isMe ? 0 : 12),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
@@ -24,7 +32,7 @@ class MessageBubble extends StatelessWidget {
           child: Text(
             message,
             style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline6.color,
+              color: isMe ? Colors.black : Theme.of(context).accentTextTheme.headline6.color,
             ),
           ),
         ),
