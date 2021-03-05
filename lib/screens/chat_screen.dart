75c1cd19d2062c0015ac481388d82647283f53f3
a7md0 <a7m3d699@gmail.com>
2021-03-05 17:47:10 +0300

    Chat sc use messages widget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -38,28 +39,14 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection(
-              '/chats/X9Ym9cELPzbUe0OAwVDX/messages',
-            )
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents as List<DocumentSnapshot>;
-
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, idx) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[idx]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
