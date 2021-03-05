4d4b97ba6f200c188018f44740226bd5134a0b03
a7md0 <a7m3d699@gmail.com>
2021-03-05 23:52:30 +0300

    Firebase initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:chat_app/screens/auth_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
