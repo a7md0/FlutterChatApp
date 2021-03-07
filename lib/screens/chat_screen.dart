4f48181cedb709c6230a1c2010a5ff14d52915d7
a7md0 <a7m3d699@gmail.com>
2021-03-07 17:23:38 +0300

    Remove unused import

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,7 +3,6 @@ import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
